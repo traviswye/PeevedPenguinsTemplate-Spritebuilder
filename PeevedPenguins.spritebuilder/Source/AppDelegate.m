d16f1f7063db8c64b88a580988ada5710768290f
Travis Wye <Twye@Traviss-MacBook-Air-2.local>
2014-06-25 10:44:11 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -54,10 +54,7 @@
     
     [self setupCocos2dWithOptions:cocos2dSetup];
     
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
 
-    
     return YES;
 }
 
--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -8,16 +8,6 @@
 
 #import "Penguin.h"
 
-@implementation Penguin
 
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    
-    return self;
-}
 
 @end
