75f70e927f1d1dcf3e334223cffbab4582eaff92
Travis Wye <Twye@Traviss-MacBook-Air-2.local>
2014-06-25 12:18:57 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,6 +12,7 @@
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
+    CCNode *_levelNode;
     CCNode *_catapultArm;
 }
 
@@ -19,6 +20,8 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 // called on every touch in this scene
